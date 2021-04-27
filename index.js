de41cab38d94294e64b424c45ceaf59c17ef0b30
alexkad58 <alexkad58@gmail.com>
2021-04-27 05:03:36 +0300

    init

---
+++ index.js
@@ -0,0 +1,26 @@
+const Discord = require('discord.js')
+const WOKComamnds = require('wokcommands')
+require('dotenv').config()
+
+const client = new Discord.Client({
+    partials: ['CHANNEL', 'MESSAGE', 'REACTION']
+})
+
+client.on('ready', () => {
+    console.log('\n Клиент запущен!\n')
+
+    new WOKComamnds(client, {
+        commandsDir: 'commands',
+        featuresDir: 'features',
+        messagesPath: '',
+        showWarns: false,
+        del: 3,
+        testServers: ['834198040843255850'] // bot owner's test server
+    })
+    .setDefaultPrefix('hhb!')
+    .setColor(0xffddff)
+    .setMongoPath(process.env.MONGO_URI)
+    .setBotOwner(['298137115877965844', '302151237674205184']) // bot owner's user id
+})
+
+client.login(process.env.TOKEN)

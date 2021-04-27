a2995ffa058afebf935421683333944753fe33f5
alexkad58 <>
2021-04-27 19:44:35 +0000

    replit init

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 const Discord = require('discord.js')
 const WOKComamnds = require('wokcommands')
 require('dotenv').config()
-require('./server')
+const express = require('express');
+    const app = express();
+    const port = 3000;
+    app.listen(port, () => console.log(`Сервер запущен - http://localhost:${port}`))
 
 const client = new Discord.Client({
-    partials: ['CHANNEL', 'MESSAGE', 'REACTION']
+  partials: ['CHANNEL', 'MESSAGE', 'REACTION']
 })
 
 client.on('ready', () => {
-    console.log('\n Клиент запущен!\n')
+  console.log('\n Клиент запущен!\n')
 
-    new WOKComamnds(client, {
-        commandsDir: 'commands',
-        featuresDir: 'features',
-        messagesPath: '',
-        showWarns: false,
-        del: 3,
-        testServers: ['834198040843255850'] // bot owner's test server
-    })
-    .setDefaultPrefix('hhb!')
+  new WOKComamnds(client, {
+    commandsDir: 'commands',
+    featuresDir: 'features',
+    messagesPath: '',
+    showWarns: false,
+    del: 3,
+    testServers: ['834198040843255850'] // bot owner's test server
+  })
+    .setDefaultPrefix('pd!')
     .setColor(0xffddff)
     .setMongoPath(process.env.MONGO_URI)
     .setBotOwner(['298137115877965844', '302151237674205184']) // bot owner's user id
